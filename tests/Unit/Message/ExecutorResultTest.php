15db7feed3924a512ef54db6ade517c7e02c5c29
Athanasios Ntavelis <davelis89@gmail.com>
2020-04-23 16:04:19 +0300

    adding test

---
+++ tests/Unit/Message/ExecutorResultTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Message;
+
+use Ntavelis\Dockposer\Enum\ExecutorStatus;
+use Ntavelis\Dockposer\Message\ExecutorResult;
+use PHPUnit\Framework\TestCase;
+
+class ExecutorResultTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->message = new ExecutorResult('create file successfully', ExecutorStatus::SUCCESS);
+    }
+
+    /** @test */
+    public function itCanReturnAResultAndAStatus(): void
+    {
+        $this->assertSame('create file successfully',$this->message->getResult());
+        $this->assertSame(ExecutorStatus::SUCCESS,$this->message->getStatus());
+    }
+}
